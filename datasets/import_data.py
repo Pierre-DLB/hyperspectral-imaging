64f4b20c220decf4556d7fd9fd6b3fc351862853
Pierre-DLB <pierre.collin-de-la-belliere>
2024-05-06 21:30:14 +0200

    dataset downloading and setup

---
+++ datasets/import_data.py
@@ -0,0 +1,37 @@
+import os
+import yaml
+import requests
+from tqdm import tqdm
+
+
+def download_file(url, filename):
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get("Content-Length", 0))
+
+    with open(filename, "wb") as file, tqdm(
+        desc=filename,
+        total=total_size,
+        unit="iB",
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as progress_bar:
+        for data in response.iter_content(chunk_size=1024):
+            size = file.write(data)
+            progress_bar.update(size)
+
+
+def get_data():
+    with open("config_data.yaml", "r") as file:
+        config = yaml.safe_load(file)
+
+    base_url = config["url"]
+    sources = ["IP", "PU", "KSC"]
+
+    for source in sources:
+        image_url = base_url[source]["original"]
+        filename = f"{source}_original.mat"
+        download_file(image_url, filename)
+
+
+if __name__ == "__main__":
+    get_data()
