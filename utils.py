8c08a86b9f4f5bb77ce3aa55e615e90727045840
Pierre-DLB <pierre.collin-de-la-belliere>
2024-05-16 17:33:32 +0200

    auto encoders

--- utils.py
+++ utils.py
@@ -60,3 +60,32 @@ def extract_tiles(X, number, w):
     for a, b in patches_top_left_corners:
         X_patches.append(X[a : a + w, b : b + w, :])
     return np.array(X_patches)
+
+
+def random_split(y, test_size=0.2):
+    """Returns the mask for a balanced split train-test
+    y : labels
+    test_size : proportion of the test set
+
+    returns : train_mask, test_mask, unsup_mask
+
+    """
+    unsup_mask = y == 0  # some pixels are not that usefull : did not get any label
+
+    train_mask = y == -1  # false everywhere, shaped as y
+    test_mask = y == -1  # false everywhere, shaped as y
+
+    col, rows = np.where(y != 0)
+    col_train, col_test, row_train, row_test = train_test_split(
+        col, rows, test_size=test_size
+    )
+    train_mask[col_train, row_train] = True
+    test_mask[col_test, row_test] = True
+    return train_mask, test_mask, unsup_mask
+
+
+def data_split(y, test_size=0.2, style="random"):
+    if style == "balanced":
+        return balanced_split(y, test_size)
+    else:
+        return random_split(y, test_size)
